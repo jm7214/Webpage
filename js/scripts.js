e122d868a1ad430ba680b8c96a43cf240143ed76
jm7214 <60760913+jm7214@users.noreply.github.com>
2020-02-13 11:10:02 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 
-$('#apartment-view').css('background-image', `url(${aptImageUrl})`)
-
-  $('#apartment-view').empty()
-
-$('.apt-list-item').on('click', function() {
+jQuery('.apt-list-item').on('click', function() {
 	// pull out the album cover url from the element's attributes
 	var aptImageUrl = $(this).attr('data-apt-image')
   // log the album cover url to the console
   console.log(aptImageUrl)
+
+    jQuery('#apartment-view').empty()
+
+    jQuery('#apartment-view').css('background-image', `url(${aptImageUrl})`)
 
 })
